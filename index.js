a9a375e56b42a3cdd8d65a0511aacce38447bba7
clement <maboiteaspam@wanadoo.fr>
2015-12-05 15:03:23 +0800

    add mdLine

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 
+var util = require('util');
 var marked = require('marked');
 var TerminalRenderer = require('marked-terminal');
 
-marked.setOptions({
-  renderer: new TerminalRenderer()
-});
+module.exports = function (renderOpts) {
 
-module.exports = function () {
+  renderOpts = renderOpts || {};
+
   if(!console.md) {
+    var renderer = new TerminalRenderer(renderOpts);
     console.md = function (msg) {
+      marked.setOptions({
+        renderer: renderer
+      });
       console.log(
-        marked(msg)
+        marked(
+          util.format.apply(
+            util,
+            [].slice.apply(arguments)
+          )
+        )
+      )
+    }
+  }
+  if(!console.mdline) {
+    var mdLineRenderer = new TerminalRenderer(renderOpts);
+    mdLineRenderer.code = function(code, lang, escaped) {
+      var g = TerminalRenderer.prototype.code.apply(mdLineRenderer, [].slice.apply(arguments));
+      return g.replace(/^\n+/, '').replace(/\n+$/, '').replace(/`/g, '')
+    };
+    console.mdline = function (format, msg) {
+      marked.setOptions({
+        renderer: mdLineRenderer
+      });
+
+      console.log(
+        marked(
+          util.format.apply(
+            util,
+            [].slice.apply(arguments)
+          )
+        ).replace(/^\n+/, '').replace(/\s+$/, '')
       )
     }
   }
